f16909f6e3d71a31be5fbab10ff18bb80a5a5185
u <u>
2025-04-10 11:05:09 +0300

    update

--- regex.pl
+++ regex.pl
@@ -4,13 +4,16 @@ s~(Version:)\s*([^0-9\.]*)([0-9\.]*)([^\s]*)\s*~\1  \3\n%define oldver \2\3\4\n~
 s~(^%gometa.*)~%{?!tag:%{?!commit:%global tag v%{oldver}}}\n\1\n~g;
 
 my $text = << 'EOF';
+
 shopt -s globstar
+
 for u in wireguard-go netlink 
 do
   for i in go.mod go.sum
   do
     sed -i "s~github.com/tailscale/${u}~~g" "${i}"
   done
+
   for i in **/* */* 
   do 
     if test -f ${i}
@@ -18,6 +21,27 @@ do
       sed -i "s~github.com/tailscale/${u}~github.com/sagernet/${u}~g;" "${i}"
     fi
   done
+done
+
+for i in go.mod go.sum
+do
+  sed -i "s~github.com/tailscale/xnet~~g" "${i}"
+done
+
+for i in **/* */*
+do
+  if test -f ${i}
+  then
+    sed -i "s~github.com/tailscale/xnet~golang.org/x/net~g;" "${i}"
+  fi
+done
+
+for i in **/* */*
+do
+  if test -f ${i}
+  then
+    sed -i "s~github.com/illarion/gonotify/v2~github.com/illarion/gonotify/v3~g;" "${i}"
+  fi
 done
 
 EOF
